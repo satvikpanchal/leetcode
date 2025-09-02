58d311ff4377debf44686e30b2321ea850a9b915
satvikpanchal <panchalsatvik@gmail.com>
2025-09-02 00:21:07 -0700

    first attempt is subsequence

---
+++ is_subsequence.py
@@ -0,0 +1,10 @@
+# first attempt
+
+# subs_list = list(s)
+# t_list = list(t)
+# filtered = [ch for ch in t_list if ch in subs_list]
+# print(subs_list)
+# print(filtered)
+# return filtered == subs_list if len(filtered) == len(subs_list) else False
+
+# second attempt
