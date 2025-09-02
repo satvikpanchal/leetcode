42532ffb885b81352510ace6f9c2a2d621d567eb
satvikpanchal <panchalsatvik@gmail.com>
2025-09-01 21:01:44 -0700

    second attempt longest substring

--- longest_substring.py
+++ longest_substring.py
@@ -1,30 +1,51 @@
-# first attempt
+# # first attempt
 
-from collections import defaultdict
+# from collections import defaultdict
 
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        substrings = []
-        cur_freq_dict = defaultdict(int)
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         substrings = []
+#         cur_freq_dict = defaultdict(int)
 
-        if len(s) == 1:
-            return 1
+#         if len(s) == 1:
+#             return 1
 
-        for i in s:
-            if i not in cur_freq_dict:
-                cur_freq_dict[i] = 1
-            else:
-                substrings.append(cur_freq_dict.copy())
-                cur_freq_dict.clear()
-                cur_freq_dict[i] = 1
+#         for i in s:
+#             if i not in cur_freq_dict:
+#                 cur_freq_dict[i] = 1
+#             else:
+#                 substrings.append(cur_freq_dict.copy())
+#                 cur_freq_dict.clear()
+#                 cur_freq_dict[i] = 1
 
-        if cur_freq_dict:
-            substrings.append(cur_freq_dict.copy())
+#         if cur_freq_dict:
+#             substrings.append(cur_freq_dict.copy())
 
-        print(substrings)
+#         print(substrings)
 
-        max_len = 0
-        for j in substrings:
-            max_len = max(len(j), max_len)
+#         max_len = 0
+#         for j in substrings:
+#             max_len = max(len(j), max_len)
 
-        return max_len
+#         return max_len
+
+
+# second attempt
+
+subs_set = set()
+l = 0
+
+max_len = 0
+
+for r in range(0, len(s)):
+    while s[r] in subs_set:
+        subs_set.remove(s[l])
+
+        # Updating the left window thing
+        l += 1
+
+    subs_set.add(s[r])
+
+    max_len = max(max_len, len(subs_set))
+
+# return max_len
