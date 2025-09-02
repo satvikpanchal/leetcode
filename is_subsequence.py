b22f26cbc95781edc3f5d84e881885cfaa31b3c3
satvikpanchal <panchalsatvik@gmail.com>
2025-09-02 00:24:11 -0700

    second attempt is subsequence

--- is_subsequence.py
+++ is_subsequence.py
@@ -8,3 +8,17 @@
 # return filtered == subs_list if len(filtered) == len(subs_list) else False
 
 # second attempt
+
+i, j = 0, 0
+
+while i < len(s) and j < len(t):
+    if s[i] == t[j]:
+        i += 1
+        j += 1
+    else:
+        j += 1
+
+if i == len(s):
+    return True
+else:
+    return False
