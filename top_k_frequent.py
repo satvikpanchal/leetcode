9d4b8a32536f53d91ed79b4eb949f5a6b5438e0c
satvikpanchal <panchalsatvik@gmail.com>
2025-09-05 23:29:46 -0700

    top k frequent element

---
+++ top_k_frequent.py
@@ -0,0 +1,21 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        sorted_list = nums
+        freq_dict = {}
+
+        for i in sorted_list:
+            if i in freq_dict:
+                freq_dict[i] += 1
+            else:
+                freq_dict[i] = 1
+
+        sorted_values = sorted(freq_dict.items(), key=lambda x: x[1], reverse=True)
+
+        final_list = []
+
+        for i in sorted_values[0:k]:
+            final_list.append(i[0])
+
+
+        return final_list
+        
